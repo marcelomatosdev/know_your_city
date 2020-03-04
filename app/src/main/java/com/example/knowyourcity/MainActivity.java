5ef98f5495d1b9aca173032f8511ff7b124e548b
Marcelo Matos <marcelomatos.dev@gmail.com>
2020-03-04 14:26:10 -0600

    initial commit

--- app/src/main/java/com/example/knowyourcity/MainActivity.java
+++ app/src/main/java/com/example/knowyourcity/MainActivity.java
@@ -5,12 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
@@ -27,9 +28,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView introImage;
     private TextView placeName;
     private TextView placeText;
+    private TextView placeURL;
 
     public int[][] places;
     private int position=0;
+    private int lastPosition;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -44,24 +47,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnNext = findViewById(R.id.btnNext);
         placeName = findViewById(R.id.textView);
         placeText = findViewById(R.id.textView2);
+        placeURL = findViewById(R.id.textView3);
+        placeURL.setMovementMethod(LinkMovementMethod.getInstance());
+        placeURL.setLinkTextColor(Color.parseColor("#C34879"));
+
         btnNext.setOnClickListener(this);
 
         places = new int[][]{
-            new int[]{R.string.cmhr_name, R.string.cmhr_text, R.drawable.cmhr},
-            new int[]{R.string.circleoflifethunderbirdhouse_name, R.string.circleoflifethunderbirdhouse_text, R.drawable.circleoflifethunderbirdhouse},
-            new int[]{R.string.backalleyarctic_name, R.string.backalleyarctic_text, R.drawable.backalleyarctic},
-            new int[]{R.string.manitobalegislativebuilding_name, R.string.manitobalegislativebuilding_text, R.drawable.manitobalegislativebuilding},
-            new int[]{R.string.esplanaderielpedestrianbridge_name, R.string.esplanaderielpedestrianbridge_text, R.drawable.esplanaderielpedestrianbridge},
-            new int[]{R.string.stbonifacecathedral_name, R.string.stbonifacecathedral_text, R.drawable.stbonifacecathedral},
-            new int[]{R.string.universityofmanitoba_name, R.string.universityofmanitoba_text, R.drawable.universityofmanitoba},
+            new int[]{R.string.cmhr_name, R.string.cmhr_text, R.drawable.cmhr,R.string.cmhr_URL},
+            new int[]{R.string.circleoflifethunderbirdhouse_name, R.string.circleoflifethunderbirdhouse_text, R.drawable.circleoflifethunderbirdhouse,R.string.circleoflifethunderbirdhouse_URL},
+            new int[]{R.string.backalleyarctic_name, R.string.backalleyarctic_text, R.drawable.backalleyarctic,R.string.backalleyarctic_URL},
+            new int[]{R.string.manitobalegislativebuilding_name, R.string.manitobalegislativebuilding_text, R.drawable.manitobalegislativebuilding,R.string.manitobalegislativebuilding_URL},
+            new int[]{R.string.esplanaderielpedestrianbridge_name, R.string.esplanaderielpedestrianbridge_text, R.drawable.esplanaderielpedestrianbridge,R.string.esplanaderielpedestrianbridge_URL},
+            new int[]{R.string.stbonifacecathedral_name, R.string.stbonifacecathedral_text, R.drawable.stbonifacecathedral,R.string.stbonifacecathedral_URL},
+            new int[]{R.string.universityofmanitoba_name, R.string.universityofmanitoba_text, R.drawable.universityofmanitoba,R.string.universityofmanitoba_URL},
         };
     }
    public void onClick(View view){
+       lastPosition = position;
 
-       int lastPosition = position;
+
        Random random = new Random();
-       position = random.nextInt(places.length);
 
+       position = random.nextInt(places.length);
        if (position == lastPosition){
            position = random.nextInt(places.length);
        }
@@ -71,6 +79,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
        placeName.setText(places[position][0]);
        placeText.setText(places[position][1]);
        introImage.setImageResource(places[position][2]);
+       placeURL.setText(places[position][3]);
    }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
@@ -88,4 +97,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             altLayout = false;
         }
     }
+
+
 }
